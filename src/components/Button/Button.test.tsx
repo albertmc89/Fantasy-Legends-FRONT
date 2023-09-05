eae0e9fa3644f1d8694fb566977dd4252466b260
Albert Colubi <albertmc89@gmail.com>
2023-09-05 15:26:54 +0200

    Test Button component

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter } from "react-router-dom";
+import Button from "./Button";
+import { render, screen } from "@testing-library/react";
+
+describe("Given a Button component", () => {
+  describe("When it is rendered", () => {
+    test("It should show a text 'Log out'", () => {
+      const mockFunction = vi.fn();
+      const buttonText = "Log out";
+
+      render(
+        <BrowserRouter>
+          <Button
+            className=""
+            actionOnClick={mockFunction}
+            text="Log out"
+          ></Button>
+        </BrowserRouter>,
+      );
+
+      const button = screen.getByRole("button", { name: buttonText });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
