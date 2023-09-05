2acec0693a7b475f50b49979bf9cedc4ee9c0818
Albert Colubi <albertmc89@gmail.com>
2023-09-05 17:57:08 +0200

    Add HomePage with login

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
+import HomePage from "../../pages/HomePage/HomePage";
 import Header from "../Header/Header";
 
 const App = (): React.ReactElement => {
   return (
     <div className="container">
       <Header />
+      <HomePage />
     </div>
   );
 };
