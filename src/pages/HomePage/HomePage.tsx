fee1028a9e2c91a7fb9064e9f3b9412e608cdb5e
Albert Colubi <albertmc89@gmail.com>
2023-09-06 18:47:35 +0200

    Add routes in App and refactor navigate in HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,45 +1,39 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import { auth, gitHubProvider } from "../../firebase";
 import "./HomePage.css";
 import { signInWithPopup } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-import PlayersListPage from "../PlayersListPage/PlayersListPage";
 
 const HomePage = () => {
-  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   const login = async () => {
     await signInWithPopup(auth, gitHubProvider);
-
-    <Navigate to="/home" />;
+    navigate("/players");
   };
 
   return (
     <>
-      {!user && (
-        <section className="main-container">
-          <div className="login-container">
-            <div className="login-container__avatar">
-              <img
-                className="login-container__image"
-                src="./img/logo-avatar.png"
-                alt="imagen avatar traje"
-              />
-            </div>
-            <div className="login-container__title-container">
-              <h2 className="login-container__title">Welcome</h2>
-              <p>Access with your Github account</p>
-              <Button
-                className="button black-login"
-                text="Log in"
-                actionOnClick={login}
-              />
-            </div>
+      <section className="main-container">
+        <div className="login-container">
+          <div className="login-container__avatar">
+            <img
+              className="login-container__image"
+              src="./img/logo-avatar.png"
+              alt="imagen avatar traje"
+            />
           </div>
-        </section>
-      )}
-      {user && <PlayersListPage />}
+          <div className="login-container__title-container">
+            <h2 className="login-container__title">Welcome</h2>
+            <p>Access with your Github account</p>
+            <Button
+              className="button black-login"
+              text="Log in"
+              actionOnClick={login}
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
