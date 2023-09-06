2f40d19bbd455fd32d972648d8508f0b13fbe351
Albert Colubi <albertmc89@gmail.com>
2023-09-06 16:03:43 +0200

    Add firebaseConfig and enviornment varibles

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GithubAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_API_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_API_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_API_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_API_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_API_APPID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export const gitHubProvider = new GithubAuthProvider();
