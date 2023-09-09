6b02ec77bbc1b8defb296d8f521b503d94aaa203
Albert Colubi <albertmc89@gmail.com>
2023-09-09 20:57:38 +0200

    Install and config msw for testing

--- src/mocks/playersMock.ts
+++ src/mocks/playersMock.ts
@@ -1,4 +1,4 @@
-import { Player } from "../types";
+import { ApiPlayers, Player } from "../types";
 
 export const playersMock: Player[] = [
   {
@@ -28,3 +28,34 @@ export const playersMock: Player[] = [
     isBought: false,
   },
 ];
+
+export const apiMockPlayers: ApiPlayers = {
+  players: [
+    {
+      _id: "1",
+      name: "Leo Messi",
+      country: "Argentina",
+      age: 36,
+      height: 169,
+      goals: 818,
+      games: 1038,
+      position: "ST",
+      image:
+        "https://cdn.discordapp.com/attachments/1149732795334266962/1149735198225858581/Lionel-Messi.webp",
+      isBought: true,
+    },
+    {
+      _id: "2",
+      name: "Thierry Henry",
+      country: "France",
+      age: 42,
+      height: 188,
+      goals: 416,
+      games: 922,
+      position: "ST",
+      image:
+        "https://cdn.discordapp.com/attachments/1149732795334266962/1149733956523147324/thierry-henry.webp",
+      isBought: false,
+    },
+  ],
+};
