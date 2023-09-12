c6442f25856e43e5bd28df1da6920b79b39e0572
Albert Colubi <albertmc89@gmail.com>
2023-09-12 22:16:08 +0200

    Add toast to showFeedbak and styles

---
+++ src/components/FeedBack/showFeedBack.ts
@@ -0,0 +1,28 @@
+import { toast } from "react-toastify";
+import "./showFeedBack.css";
+
+export const showFeedBack = (text: string, success: boolean) => {
+  success
+    ? toast.success(text, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        className: "success",
+      })
+    : toast.error(text, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        className: "error",
+      });
+};
