e7fc8255ae78448599bc43a9a4afb4211054f721
Albert Colubi <albertmc89@gmail.com>
2023-09-15 10:47:02 +0200

    Test addPlayer action

---
+++ src/store/players/__tests__/addPlayer.test.tsx
@@ -0,0 +1,22 @@
+import { playersCreatedMock, playersMock } from "../../../mocks/playersMock";
+import { PlayerState } from "../../types";
+import { addPlayerActionCreator, playersReducer } from "../playersSlice";
+
+describe("Given a playersReducer reducer", () => {
+  describe("When it receives a state with one user and addPlayer action", () => {
+    test("Then it should return a new state with the player with id 11 added", () => {
+      const currentPlayersState: PlayerState = {
+        players: playersMock,
+      };
+
+      const addPlayerAction = addPlayerActionCreator(playersCreatedMock);
+
+      const newUserState = playersReducer(currentPlayersState, addPlayerAction);
+
+      expect(newUserState.players).toContain(playersCreatedMock);
+      expect(newUserState.players).toHaveLength(
+        currentPlayersState.players.length + 1,
+      );
+    });
+  });
+});
