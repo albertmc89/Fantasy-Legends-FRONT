9f88444b8ce00af9b6ddea2706d91e4b7639ab8c
Albert Colubi <albertmc89@gmail.com>
2023-09-15 13:22:04 +0200

    Add onSubmit function on NewPlayerForm

--- src/pages/NewPlayerPage/NewPlayerPage.tsx
+++ src/pages/NewPlayerPage/NewPlayerPage.tsx
@@ -2,13 +2,13 @@ import NewPlayerForm from "../../components/NewPlayerForm/NewPlayerForm";
 import "./NewPlayerPage.css";
 
 const NewPlayerPage = () => {
+  const onSubmitPlayer = () => {};
+
   return (
-    <>
-      <div className="addplayer">
-        <h2 className="title">Add player</h2>
-        <NewPlayerForm />
-      </div>
-    </>
+    <div className="addplayer">
+      <h2 className="title">Add player</h2>
+      <NewPlayerForm onSubmitPlayer={onSubmitPlayer} />
+    </div>
   );
 };
 
