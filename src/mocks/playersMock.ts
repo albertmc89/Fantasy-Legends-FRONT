1ab190984b9fa0d5768b50d8afe0de0dab969419
Albert Colubi <albertmc89@gmail.com>
2023-09-16 23:17:51 +0200

    Test addPlayerApi hook

--- src/mocks/playersMock.ts
+++ src/mocks/playersMock.ts
@@ -64,8 +64,8 @@ export const apiMockPlayers: ApiPlayer[] = [
 
 export const idPlayerMock = playersMock[0].id;
 
-export const playersCreatedMock = {
-  id: "1",
+export const playerCreatedApiMock = {
+  id: "650030d670bf0a89283a4ac5",
   name: "Leo Messi",
   country: "Argentina",
   age: 36,
@@ -78,3 +78,33 @@ export const playersCreatedMock = {
   isBought: true,
   user: "userId",
 };
+
+export const playerCreatedMock = {
+  name: "Leo Messi",
+  country: "Argentina",
+  age: 36,
+  height: 169,
+  goals: 818,
+  games: 1038,
+  position: "ST",
+  image:
+    "https://cdn.discordapp.com/attachments/1149732795334266962/1149735198225858581/Lionel-Messi.webp",
+  isBought: true,
+};
+
+export const createdPlayerForm: ApiPlayer[] = [
+  {
+    _id: "1",
+    name: "Leo Messi",
+    country: "Argentina",
+    age: 36,
+    height: 169,
+    goals: 818,
+    games: 1038,
+    position: "ST",
+    image:
+      "https://cdn.discordapp.com/attachments/1149732795334266962/1149735198225858581/Lionel-Messi.webp",
+    isBought: true,
+    user: "userId",
+  },
+];
